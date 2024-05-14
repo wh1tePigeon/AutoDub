5298c4ff6a8aebc2a895da57a49a5a6a77bee21f
GYO <nickizbig@gmail.com>
2024-05-14 16:50:45 +0300

    upd

--- source/inference/__init__.py
+++ source/inference/__init__.py
@@ -1,5 +1,5 @@
 from source.inference.asr.inference_asr import inference_asr
 from source.inference.bsrnn.inference_bsrnn import inference_bsrnn
 from source.inference.vad.inference_vad import inference_vad
-from source.inference.translate.translate_google import translate_file_google
+from source.inference.translate.inference_translate import translate_file_google
 from source.inference.tts.inference_tts_lazy import lazy_tts
