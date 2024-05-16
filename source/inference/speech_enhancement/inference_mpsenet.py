f1c9a5aeea609e3725f14f328fd4554b0ce348eb
GYO <nickizbig@gmail.com>
2024-05-16 23:12:10 +0300

    upd

--- source/inference/speech_enhancement/inference_mpsenet.py
+++ source/inference/speech_enhancement/inference_mpsenet.py
@@ -5,6 +5,7 @@ import hydra
 import sys
 from pathlib import Path
 import torchaudio as ta
+import torch.nn.functional as F
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent.parent))
 from source.model.mpsenet.generator import MPNet
 from source.datasets.mpsenet.dataset import mag_pha_stft, mag_pha_istft
@@ -12,7 +13,7 @@ from source.utils.util import prepare_device
 from source.utils.util import CONFIGS_PATH
 
 
-CONFIG_MPSNET = CONFIGS_PATH / 'mpsnet'
+CONFIG_MPSENET = CONFIGS_PATH / 'mpsenet'
 
 
 def inference_mpsenet(cfg):
@@ -23,11 +24,13 @@ def inference_mpsenet(cfg):
         model = torch.nn.DataParallel(model, device_ids=device_ids)
 
     checkpoint = torch.load(cfg["checkpoint_path"], map_location=device)
-    state_dict = checkpoint["state_dict"]
+    state_dict = checkpoint["generator"]
     model.load_state_dict(state_dict)
     model.eval()
 
     filepath = cfg["filepath"]
+    assert os.path.exists(filepath)
+
     output_dir = cfg["output_dir"]
     sr = cfg["sampling_rate"]
 
@@ -41,12 +44,31 @@ def inference_mpsenet(cfg):
         audio = audio.to(device)
 
         with torch.inference_mode():
-            norm_factor = torch.sqrt(len(audio) / torch.sum(audio ** 2.0)).to(device)
-            audio = (audio * norm_factor).unsqueeze(0)
-            noisy_amp, noisy_pha, noisy_com = mag_pha_stft(audio, **cfg["stft"])
+            # norm_factor = torch.sqrt(len(audio) / torch.sum(audio ** 2.0)).to(device)
+            # audio = audio * norm_factor
+
+            cfg_ft = {
+                "n_fft" : cfg["n_fft"],
+                "hop_size" : cfg["hop_size"],
+                "win_size" : cfg["win_size"],
+                "compress_factor" : cfg["compress_factor"]
+            }
+
+            noisy_amp, noisy_pha, noisy_com = mag_pha_stft(audio, **cfg_ft)
             amp_g, pha_g, com_g = model(noisy_amp, noisy_pha)
-            audio_clean = mag_pha_istft(amp_g, pha_g, **cfg["stft"])
-            speech = audio_clean / norm_factor
+            audio_clean = mag_pha_istft(amp_g, pha_g, **cfg_ft)
+            #speech = audio_clean / norm_factor
+            # audio = audio / norm_factor
+
+            speech = audio_clean
+            background_len = audio.shape[-1]
+            speech_len = speech.shape[-1]
+
+            if background_len <= speech_len:
+                speech = speech[:, :background_len]
+            else:
+                padding_size = background_len - speech_len
+                speech = F.pad(speech, pad=(0, padding_size), mode='constant', value=0)
 
             background = audio - speech
 
@@ -57,18 +79,21 @@ def inference_mpsenet(cfg):
             
             speech_save_path = os.path.join(directory_save_file, (filename + "_speech.wav"))
             background_save_path = os.path.join(directory_save_file, (filename + "_background.wav"))
+            audio_save_path = os.path.join(directory_save_file, (filename + "_orig.wav"))
 
             speech = speech.to("cpu")
             background = background.to("cpu")
 
             ta.save(speech_save_path, speech, sample_rate=sr)
             ta.save(background_save_path, background, sample_rate=sr)
+            ta.save(audio_save_path, audio, sample_rate=sr)
 
             return [speech_save_path, background_save_path]
 
 
-@hydra.main(config_path=str(CONFIG_MPSNET), config_name="inf")
+@hydra.main(config_path=str(CONFIG_MPSENET), config_name="inf")
 def main(cfg):
+    cfg["filepath"] = "/home/comp/Рабочий стол/RaM/segment_2_cutted.wav"
     inference_mpsenet(cfg)
 
 
