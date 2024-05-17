0224286c1ef366ecd9d50972caf79d5aa78396af
GYO <nickizbig@gmail.com>
2024-05-17 22:10:35 +0300

    upd

--- source/utils/process_mkv.py
+++ source/utils/process_mkv.py
@@ -9,10 +9,50 @@ import numpy as np
 from moviepy.editor import VideoFileClip, AudioFileClip
 from pydub import AudioSegment
 from tqdm import tqdm
+import ffmpeg
 from speechbrain.inference.enhancement import SpectralMaskEnhancement, WaveformEnhancement
+from moviepy.editor import VideoFileClip
 
 
-def process_mkv_file(filepath, output_dir):
+def process_mkv_file(filepath, output_dir, languages, extract_srt=True, extract_video=False):
+    assert os.path.exists(filepath)
+    
+    filename = filepath.split(".")[0].split("/")[-1]
+    os.makedirs(output_dir, exist_ok=True)
+
+    stream_info = ffmpeg.probe(filepath)
+    tracks_info = stream_info['streams']
+
+    audio_tracks = []
+
+    if extract_srt:
+        subs_tracks = []
+
+    if extract_video:
+        video_tracks = []
+    
+    for track in tracks_info:
+        if track["codec_type"] == "audio":
+            if track["tags"]["language"] in languages:
+                audio_tracks.append(track)
+        
+        if extract_video and track["codec_type"] == "video":
+            video_tracks.append(track)
+
+        if extract_srt and track["codec_type"] == "subtitle":
+            if track["tags"]["language"] in languages:
+                subs_tracks.append(track)
+
+    for track in audio_tracks:
+        track_filename = filename + "_" + track["tags"]["language"] + ".wav"
+        stream = ffmpeg.input(filepath, ss=None, t=-1, select_streams=f'[stream={track["index"]}]:kind={track["codec_type"]}')
+        ffmpeg.output(stream, track_filename).run()
+
+
+    #return audios
+    
+    
+    #stream1 = ffmpeg.input(filepath)
     return 0
 
 
@@ -22,8 +62,9 @@ def process_mkv_dir(filepath, output_dir):
 
 if __name__ == "__main__":
     cfg = {
-        "filepath" : "",
-        "output_dir" : ""
+        "filepath" : "/home/comp/Рабочий стол/RaM/rm1.mkv",
+        "languages": ["eng", "rus"],
+        "output_dir" : "/home/comp/Рабочий стол/ffmpeg"
     }
 
     process_mkv_file(**cfg)
