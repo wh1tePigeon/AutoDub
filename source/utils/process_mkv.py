d788c7704949e38b47ac270969465add26121dd5
GYO <nickizbig@gmail.com>
2024-05-17 22:38:30 +0300

    upd

--- source/utils/process_mkv.py
+++ source/utils/process_mkv.py
@@ -10,13 +10,14 @@ from moviepy.editor import VideoFileClip, AudioFileClip
 from pydub import AudioSegment
 from tqdm import tqdm
 import ffmpeg
+#from ffmpeg import FFmpeg
 from speechbrain.inference.enhancement import SpectralMaskEnhancement, WaveformEnhancement
 from moviepy.editor import VideoFileClip
 
 
 def process_mkv_file(filepath, output_dir, languages, extract_srt=True, extract_video=False):
     assert os.path.exists(filepath)
-    
+
     filename = filepath.split(".")[0].split("/")[-1]
     os.makedirs(output_dir, exist_ok=True)
 
@@ -30,7 +31,7 @@ def process_mkv_file(filepath, output_dir, languages, extract_srt=True, extract_
 
     if extract_video:
         video_tracks = []
-    
+        
     for track in tracks_info:
         if track["codec_type"] == "audio":
             if track["tags"]["language"] in languages:
@@ -43,20 +44,39 @@ def process_mkv_file(filepath, output_dir, languages, extract_srt=True, extract_
             if track["tags"]["language"] in languages:
                 subs_tracks.append(track)
 
+    ff = ffmpeg.input(filepath)
+    audio_save_paths = []
+    subs_save_paths = []
+    video_save_paths = []
+
     for track in audio_tracks:
-        track_filename = filename + "_" + track["tags"]["language"] + ".wav"
-        stream = ffmpeg.input(filepath, ss=None, t=-1, select_streams=f'[stream={track["index"]}]:kind={track["codec_type"]}')
-        ffmpeg.output(stream, track_filename).run()
-
-
-    #return audios
+        track_filename = filename + "_audio_" + str(track["index"]) + "_" + track["tags"]["language"] + ".wav"
+        track_filepath = os.path.join(output_dir, track_filename)
+        audio_save_paths.append(track_filepath)
+        m = "0:" + str(track["index"])
+        ff.output(track_filepath, **{"map": m}).run()
     
-    
-    #stream1 = ffmpeg.input(filepath)
-    return 0
+    if extract_srt:
+        for track in subs_tracks:
+            track_filename = filename + "_subs_" + str(track["index"]) + "_" + track["tags"]["language"] + ".srt"
+            track_filepath = os.path.join(output_dir, track_filename)
+            subs_save_paths.append(track_filepath)
+            m = "0:" + str(track["index"])
+            ff.output(track_filepath, **{"map": m}).run()
+
+    if extract_video:
+        for track in video_tracks:
+            track_filename = filename + "_video_" + str(track["index"]) + ".mp4"
+            track_filepath = os.path.join(output_dir, track_filename)
+            video_save_paths.append(track_filepath)
+            m = "0:" + str(track["index"])
+            ff.output(track_filepath, **{"map": m}).run()
+
+    return audio_save_paths, subs_save_paths, video_save_paths
 
 
-def process_mkv_dir(filepath, output_dir):
+
+def process_mkv_dir(dirpath, output_dir):
     return 0
 
 
