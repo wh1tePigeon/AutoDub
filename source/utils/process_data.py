12047fa5f9bb3944eba27eea986c33e0d18b74b6
gyo <nickizbig@gmail.com>
2024-06-04 22:02:03 +0300

    upd

--- source/utils/process_data.py
+++ source/utils/process_data.py
@@ -5,7 +5,6 @@ import os
 from typing import Tuple
 import librosa
 import pandas as pd
-import numpy as np
 from tqdm import tqdm
 import ffmpeg
 import json
@@ -18,12 +17,14 @@ from source.utils.process_mkv import process_mkv_dir, srt_to_csv
 from source.utils.process_audio import cut_n_save
 
 from speechbrain.inference.speaker import EncoderClassifier
-import pandas as pd
 from sklearn.cluster import DBSCAN, KMeans
 from sklearn.preprocessing import StandardScaler
-
+from hydra.utils import instantiate
 from scipy.spatial.distance import cdist
 import numpy as np
+from source.utils.util import prepare_device
+from source.utils.fader import OverlapAddFader
+from omegaconf import OmegaConf
 
 
 def process_data_dir(metafilepath, output_dir):
@@ -97,14 +98,18 @@ def compute_embeddings(metafilepath):
                 for i, row in tqdm(df.iterrows(), total=len(df.index)):
                     segment_path = row["path"]
                     if os.path.exists(segment_path):
-                        segment, sr = ta.load(segment_path)
-                        embedding = classifier.encode_batch(segment).squeeze().cpu().numpy()
+                        try:
+                            segment, sr = ta.load(segment_path)
+                            embedding = classifier.encode_batch(segment).squeeze().cpu().numpy()
 
-                        filename = segment_path.split(".")[0].split("/")[-1]
-                        savepath = os.path.join(output_dir, (filename + "_embd.npy"))
-                        np.save(savepath, embedding, allow_pickle=False)
+                            filename = segment_path.split(".")[0].split("/")[-1]
+                            savepath = os.path.join(output_dir, (filename + "_embd.npy"))
+                            np.save(savepath, embedding, allow_pickle=False)
 
-                        df.at[i, "embd_path"] = savepath
+                            df.at[i, "embd_path"] = savepath
+
+                        except:
+                            pass
 
                 df.to_csv(csv_segments_filepath, sep=';', index=False, encoding='utf-8')
               
@@ -223,8 +228,8 @@ def merge_sim_segments(metafilepath):
                     sims.append(s)
                 
                 print("1")
-
-
+#[0.3693041503429413, 0.43667829036712646, 0.593411922454834, 0.6331155300140381, 0.4492175579071045, 0.31460416316986084, 0.7370620965957642, 0.6188938021659851, 0.5243847966194153, 0.6632072329521179, 0.8274864554405212, 0.7624768018722534, 0.7797215580940247, 0.5962139368057251, 0.6379070281982422, 0.7393325567245483, 0.7512869834899902, 0.7231794595718384, 0.5538026690483093, 0.725588858127594, 0.5609574317932129, 0.4644603431224823, 0.5893489122390747, 0.7249554395675659, 0.6515954732894897, 0.607807457447052, 0.7581999897956848, 0.6690129637718201, 0.6436741352081299, 0.7322248220443726, 0.6074910163879395, 0.6675061583518982, 0.715245246887207, 0.5577623248100281, 0.5378882884979248, 0.5732212066650391, 0.6923876404762268, 0.6241010427474976, 0.5194295048713684, 0.42733556032180786, 0.5472899675369263, 0.5297255516052246, 0.4830099046230316, 0.5568930506706238, 0.6803110241889954, 0.6411834955215454, 0.7649610638618469, 0.490192174911499, 0.35297560691833496, 0.6389533281326294, 0.656097412109375, 0.5047973990440369, 0.36591821908950806, 0.3938295245170593, 0.5434467792510986, 0.4685512185096741, 0.6699821949005127, 0.8642969131469727, 0.6297295689582825, ...]
+#[0.25111374258995056, 0.6367850303649902, 0.4793241024017334, 0.45974671840667725, 0.2733364701271057, 0.4527074694633484, 0.7471623420715332, 0.49548104405403137, 0.558784544467926, 0.3660196363925934, 0.80808424949646, 0.6894175410270691, 0.5065523386001587, 0.5338807106018066, 0.6331297755241394, 0.6915916204452515, 0.5722323656082153, 0.5078168511390686, 0.7348804473876953]
                 #embds = torch.stack(embds)
                 #clustering = DBSCAN(metric="cosine", eps=0.4, min_samples=10).fit(embds)
                 #labels = clustering.labels_
@@ -292,9 +297,75 @@ def group_by_label(metafilepath):
                     os.makedirs(label_savedir, exist_ok=True)
                     file_savepath = os.path.join(label_savedir, segment_path.split("/")[-1])
                     shutil.copy2(segment_path, file_savepath)
-
               
     return metafilepath
+
+
+def denoise_w_bsrnn(dirpath, cfg, output_dir=None):
+    assert os.path.exists(dirpath)
+
+    if output_dir is None:
+        output_dir = dirpath
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    device, device_ids = prepare_device(cfg["n_gpu"])
+    arch = OmegaConf.load(cfg["model"])
+    model = instantiate(arch)
+    model = model.to(device)
+    if len(device_ids) > 1:
+        model = torch.nn.DataParallel(model, device_ids=device_ids)
+
+    checkpoint = torch.load(cfg["checkpoint_path"], map_location=device)
+    state_dict = checkpoint["state_dict"]
+    model.load_state_dict(state_dict)
+    model.eval()
+    sr = cfg["sr"]
+
+    for filename in tqdm(os.listdir(dirpath), total=len(os.listdir(dirpath))):
+        if filename.endswith(".wav"):
+            filepath = os.path.join(dirpath, filename)
+            try:
+                audio, fs = ta.load(filepath)
+                if fs != sr:
+                    audio = ta.functional.resample(audio, fs, sr)
+
+                if audio.shape[0] > 1:
+                    audio = torch.mean(audio, dim=0, keepdim=True)
+        
+                # move audio to gpu
+                audio = audio.to(device)
+
+                with torch.inference_mode():
+                    def forward(audio):
+                        _, output = model({"audio": {"mixture": audio},})
+                        return output["audio"]
+                    
+                    if audio.shape[-1] / sr > 12:
+                        fader = OverlapAddFader(window_type=cfg["window_type"],
+                                                chunk_size_second=cfg["chunk_size_second"],
+                                                hop_size_second=cfg["hop_size_second"],
+                                                fs=sr,
+                                                batch_size=cfg["batch_size"])
+                        
+                        output = fader(audio, lambda a: forward(a))
+                        
+                    else:
+                        audio = audio.unsqueeze(0)
+                        output = forward(audio)
+                    
+                    speech = output["speech"]
+                    speech = speech.reshape(1, -1)
+                    speech = speech.to("cpu")
+
+                    savepath = os.path.join(output_dir, filename)
+                    ta.save(savepath, speech, sr)
+            except:
+                pass
+
+    return output_dir
+
+
 
 
 
@@ -305,9 +376,24 @@ if __name__ == "__main__":
     }
     meta_path = "/home/comp/Рабочий стол/AutoDub/output/dataset/data.json"
     csv_path = "/home/comp/Рабочий стол/AutoDub/output/dataset/sherlock-1/eng/sherlock-1_audio_2_eng_speech/test.csv"
-    #merge_sim_segments(meta_path)
-    remove_dialogues_n_small_segments(csv_path)
+    merge_sim_segments(meta_path)
+    #remove_dialogues_n_small_segments(csv_path)
     #compute_embeddings(meta_path)
     #label_embds(meta_path)
     #group_by_label(meta_path)
     #process_data_dir(**cfg)
+
+    config_dict = {
+        "n_gpu": 1,
+        "model": "/home/comp/Рабочий стол/AutoDub/source/configs/bsrnn/arch/model_conf.yaml",
+        "sr": 44100,
+        "checkpoint_path": "/home/comp/Рабочий стол/AutoDub/checkpoints/bsrnn/main.pth",
+        "window_type": "hann",
+        "chunk_size_second": 6.0,
+        "hop_size_second": 0.5,
+        "batch_size": 4
+    }
+
+    dirpath = "/home/comp/Рабочий стол/AutoDub/output/dataset/sherlock-1/eng/sherlock-1_audio_2_eng_speech/test"
+    output_dir = "/home/comp/Рабочий стол/AutoDub/output/dataset/sherlock-1/eng/sherlock-1_audio_2_eng_speech/test2"
+    #denoise_w_bsrnn(dirpath, config_dict)#, output_dir)
