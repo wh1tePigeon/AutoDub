330d3d35c7cbb9708dd3c5bc2fe04d23562b42c7
gyo <nickizbig@gmail.com>
2024-06-07 00:08:01 +0300

    upd

--- dub.py
+++ dub.py
@@ -54,19 +54,8 @@ def dub(cfg):
 
     print("Cutting audio")
     cfg["cut"]["filepath"] = cfg["asr"]["filepath"]
-    # cfg["cut"]["filepath"] = "/home/comp/Рабочий стол/AutoDub/output/asr/test2_audio_mono_speech_resampled/test2_audio_mono_speech_resampled.wav"
     cfg["cut"]["csv_filepath"] = translated_csv_path
-    # cfg["cut"]["csv_filepath"] = "/home/comp/Рабочий стол/AutoDub/output/translated/test2_audio_mono_speech_resampled_asr_labeled/2.csv"
-    # cfg["cut"]["filepath"] = "/home/comp/Рабочий стол/AutoDub/output/asr/test2_audio_mono_speech_resampled/test2_audio_mono_speech_resampled.wav"
-    #cfg["cut"]["csv_filepath"] = '/home/comp/Рабочий стол/AutoDub/output/translated/test2_audio_mono_speech_resampled_asr_labeled/test2_audio_mono_speech_resampled_asr_labeled_tr.csv'
     _, cutted_csv_path = cut_n_save_by_label(**cfg["cut"])
-
-
-    # filename = "test22"
-    # speech_path = "/home/comp/Рабочий стол/AutoDub/output/bsrnn/test2_audio_mono/test2_audio_mono_speech.wav"
-    # wvideo = True
-    # background_path = "/home/comp/Рабочий стол/AutoDub/output/bsrnn/test2_audio_mono/test2_audio_mono_background.wav"
-    # video_path = "/home/comp/Рабочий стол/AutoDub/output/video_n_audio_separated/test2/test2_video.mp4"
 
     print("TTS")
     cfg["tts"]["csv_filepath"] = cutted_csv_path
@@ -83,7 +72,6 @@ def dub(cfg):
     cfg["concatenate"]["speech_path"] = speech_path
     cfg["concatenate"]["background_path"] = background_path
     cfg["concatenate"]["csv_filepath"] = aligned_audio_csv_path
-    #cfg["concatenate"]["csv_filepath"] = '/home/comp/Рабочий стол/AutoDub/output/aligned_audio/test2/test2_audio_mono_speech_resampled_asr_labeled_tr_w_segmets_paths_tts_aligned.csv'
     cfg["concatenate"]["filename"] = filename
     cfg["concatenate"]["join_video"] = wvideo
     cfg["concatenate"]["video_path"] = video_path
